9769d5c2ccee9dcf71357a7578eae474da2890fe
devil <you@example.com>
2025-07-20 18:01:36 +0530

    Update Controller

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
-	 @GetMapping("/")
-	    public String Welcome() {
-	        return "Welcome...... :)";
-	    }
+	@GetMapping("/")
+	public String Welcome() {
+		return "Welcome...... :)";
+	}
 	
     @GetMapping("/test")
     public String hello() {
