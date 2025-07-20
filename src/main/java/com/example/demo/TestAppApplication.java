0e698eb9241233d61b43d78f28d17168fb8b318b
devil <you@example.com>
2025-07-20 16:39:59 +0530

    Deploy Test

--- src/main/java/com/example/demo/TestAppApplication.java
+++ src/main/java/com/example/demo/TestAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
 	}
