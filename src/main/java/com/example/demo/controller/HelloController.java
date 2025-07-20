f6240c8f09b68d0f2797f746efbca26bef4af254
devil <you@example.com>
2025-07-20 16:35:00 +0530

    Updated package name conflict

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,18 @@
-package com.test.TestApp.controller;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	 @GetMapping("/")
+	    public String Welcome() {
+	        return "Welcome :)";
+	    }
+	
     @GetMapping("/test")
     public String hello() {
-        return "✅ Spring Boot App Deployed on Render Successfully!";
+        return "Spring Boot App Deployed on Render Successfully!";
     }
 }
